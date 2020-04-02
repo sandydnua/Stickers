6f3d08ec7fa9ee19819d347a9b5443ccd4a5faad
Aleksandr Shmarov <ashmarov@qualityunit.com>
2020-04-02 21:07:02 +0300

    refactor: remove comments and test description

--- src/main/java/stickers/database/MemberOfGroup.java
+++ src/main/java/stickers/database/MemberOfGroup.java
@@ -6,14 +6,9 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 
-//@Getter
 @AllArgsConstructor
-//@Setter
 public class MemberOfGroup {
-//    private int id;
     public int id;
-//    private String firstName;
     public String firstName;
-//    private String lastName;
     public String lastName;
 }
